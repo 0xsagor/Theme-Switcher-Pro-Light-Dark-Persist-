a694335cff384dd9af0fd98178437f8052eb2ec0
Sagor <161376518+0xsagor@users.noreply.github.com>
2026-01-06 13:48:09 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const saved = localStorage.getItem("theme");
+if (saved === "dark") document.body.classList.add("dark");
+
+function toggleTheme() {
+  document.body.classList.toggle("dark");
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("dark") ? "dark" : "light"
+  );
+}
